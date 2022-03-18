6c655e4f79e66c2ed42ccbe25c7ea04908d9a344
Maël Primet <mael.primet@gmail.com>
2022-03-18 17:45:04 +0100

    feat: update HomePage with error code

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,44 +5,61 @@
         <ion-title>Inbox</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
         <ion-refresher-content></ion-refresher-content>
       </ion-refresher>
-      
+
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Inbox</ion-title>
         </ion-toolbar>
       </ion-header>
-      
+
       <ion-list>
-        <MessageListItem v-for="message in messages" :key="message.id" :message="message" />
+        <ion-item>
+          <ion-label>Some text</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>Some text</ion-label>
+          <ion-button slot="end">action</ion-button>
+        </ion-item>
       </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonList, IonPage, IonRefresher, IonRefresherContent, IonTitle, IonToolbar } from '@ionic/vue';
-import MessageListItem from '@/components/MessageListItem.vue';
-import { defineComponent } from 'vue';
-import { getMessages } from '@/data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonButton,
+  IonItem,
+  IonLabel,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { getMessages } from "@/data/messages";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   data() {
     return {
-      messages: getMessages()
-    }
+      messages: getMessages(),
+    };
   },
   methods: {
     refresh: (ev: CustomEvent) => {
       setTimeout(() => {
         ev.detail.complete();
       }, 3000);
-    }
+    },
   },
   components: {
     IonContent,
@@ -53,7 +70,9 @@ export default defineComponent({
     IonRefresherContent,
     IonTitle,
     IonToolbar,
-    MessageListItem
+    IonButton,
+    IonItem,
+    IonLabel,
   },
 });
 </script>
